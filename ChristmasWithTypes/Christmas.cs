67bbfce3d1794f7bd91bdb16bffaef625ede108e
matt21ferg <matt21ferguson@gmail.com>
2022-01-09 20:44:11 -0600

    fixed christmas

--- ChristmasWithTypes/Christmas.cs
+++ ChristmasWithTypes/Christmas.cs
@@ -4,11 +4,13 @@ namespace ChristmasWithTypes
     public class Christmas
     {
         public string[] Presents;
-        public string Santa { get; set; }
-        public int TreeHeight { get; set; } //TODO Make the Height property nullable
+        public string Santa = "Kris Kringle";
+        public int? TreeHeight = 10; //TODO Make the Height property nullable
 
         //TODO Make the property, "Day", type enum
-        public enum Day { /*Fill out the days of the week*/ };
+        public enum Day  {Monday, Tuesday, Wedsday, Thursday, Friday, Saturday, Sunday}
 
+                
+            
     }
 }
